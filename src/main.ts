94bf5061d3ec1c01e560ec8a30ce2b34a5830b74
liliang <liang2.li@cicc.com.cn>
2024-04-12 15:35:43 +0800

    feat: electron test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+if (window.electronApi) {
+  console.log('electron api detected');
+  console.log(window.electronApi.getTheme());
+}
+
+createApp(App).mount('#app');
